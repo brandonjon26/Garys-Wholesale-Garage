1adc0cf5cfa981e65263fe1a519e84e94e1a3f6f
Brandon Hill <brandonjon26@gmail.com>
2021-06-07 13:54:56 -0500

    added inheritance and overriding drive method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Garys_Wholesale_Garage
+namespace Garage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zero fxs = new Zero();
+            Tesla modelS = new Tesla();
+            Cessna mx410 = new Cessna();
+
+            fxs.Drive();
+            modelS.Drive();
+            mx410.Drive();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+//  Zero fxs = new Zero();
+//             Zero fx = new Zero();
+//             Tesla modleS = new Tesla();
+
+
+//             List<Zero> electronicVehicles = new List<Zero>() { fx, fxs };
---
+++ Zero.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Garage
+{
+    public class Zero  // Electric motorcycle
+    {
+        public double BatteryKWh { get; set; }
+        public string MainColor { get; set; }
+        public int MaximumOccupancy { get; set; }
+
+        public void ChargeBattery()
+        {
+            // method definition omitted
+        }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine("Vrooooom!");
+        }
+    }
+}
