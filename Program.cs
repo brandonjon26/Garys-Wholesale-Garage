6ca6d44b3270739854e2c128a77c7f6cf02b0bc8
Brandon Hill <brandonjon26@gmail.com>
2021-06-07 13:30:03 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Garys_Wholesale_Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
