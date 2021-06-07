a498ee6d70c081da5a3bf10e20a44d93adc985bf
Brandon Hill <brandonjon26@gmail.com>
2021-06-07 15:55:59 -0500

    added values to properties for each vehicle

--- Program.cs
+++ Program.cs
@@ -7,15 +7,30 @@ namespace Garage
     {
         static void Main(string[] args)
         {
-            Zero fxs = new Zero();
+            Zero fxs = new Zero()
+            {
+                BatteryKWh = 10,
+                MainColor = "black",
+                MaximumOccupancy = 2
+            };
             Tesla modelS = new Tesla()
             {
                 BatteryKWh = 100,
                 MainColor = "blue",
                 MaximumOccupancy = 5
             };
-            Cessna mx410 = new Cessna();
-            Ram model1500 = new Ram();
+            Cessna mx410 = new Cessna()
+            {
+                FuelCapacity = 57,
+                MainColor = "white",
+                MaximumOccupancy = 4
+            };
+            Ram model1500 = new Ram()
+            {
+                FuelCapacity = 25,
+                MainColor = "red",
+                MaximumOccupancy = 5
+            };
 
             fxs.Drive();
             modelS.Drive();
